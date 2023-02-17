0d237491308b8cc03186fe7fad9db513941133f6
nystagmus <ialexefimenko@gmail.com>
2023-02-17 13:39:12 +0300

    create

--- src/sc.py
+++ src/sc.py
@@ -2,7 +2,7 @@
 Subtractive Clustering Algorithm according to "Learning of fuzzy rules by mountain clustering" by Ronald R. Yager and Dimitar P. Filev.
 '''
 __author__ = 'Alex Povod'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 import numpy
 import numpy.typing as npt
@@ -11,7 +11,7 @@ import scipy as sp
 
 class SubtractiveClustering:
 
-    def __init__(self, r_1:int, r_2:float, eps = 0.5):
+    def __init__(self, r_1:float, r_2:float, eps = 0.001, verbose = False) -> None:
         '''
         Implemetation Subtractive Clustering Method for computing the cluster centroids, 
         which belongs to unsupervised learning and can quickly determine the number of clusters and 
@@ -22,7 +22,7 @@ class SubtractiveClustering:
         :param: eps:    is the accuracy ratio, positive constant less than 1.
         '''
         
-        self.r_1 = r_1; self.r_2 = r_2; self.eps = eps    
+        self.r_1 = r_1; self.r_2 = r_2; self.eps = eps; self.verbose = verbose    
         
     def fit(self, data: npt.NDArray):
         
@@ -33,19 +33,23 @@ class SubtractiveClustering:
         # Init array of number centroid clusters
         self.centers = numpy.empty((0, data.shape[1]))
         # Step 2: Select maximum mountain function
-        max = numpy.max(mountain_function)
+        max = numpy.max(self.potential)
         max_star = 0
+        count = 1
         while(max >= self.eps * max_star):
             
-            center = data[numpy.argmax(mountain_function)]
+            center = data[numpy.argmax(self.potential )]
             self.centers = numpy.vstack((self.centers, center))
             # Step 3: Update the Mountain Function
-            mountain_function = self.update_mountain_function(mountain_function, max, center, data)
+            self.potential  = self.update_mountain_function(self.potential , max, center, data)
 
             # Step 4: Update maximum mountain fuction and check in loop
             max_star = max
-            max = numpy.max(mountain_function)
+            max = numpy.max(self.potential)
             
+            if self.verbose:
+                print(f'SC iteration {count}, potential: {self.potential}')
+            count += 1
         return self
         
 
@@ -54,22 +58,21 @@ class SubtractiveClustering:
         Step 1 of algoritm compute the potential (mountain function) for each point or sample.
         returns: the potential for that given row given no previous potential known
         '''
-        potential = numpy.array([])
-        i = 0
-        while(i < len(data)):
-            ls = numpy.array([])
-            for ii in range(0, len(data)):
-                numerator = numpy.power((data[i] - data[ii]), 2)
-                denomerator = numpy.power((self.r_1 / 2),2)
-                ratio = numpy.exp(numpy.negative(numerator.sum() / denomerator))
-                ls = numpy.append(ls, ratio).sum()
-            i += 1
-            potential = numpy.append(potential, ls)
+        # potential = numpy.array([])
+        # i = 0
+        # while(i < len(data)):
+        #     ls = numpy.array([])
+        #     for ii in range(0, len(data)):
+        #         numerator = numpy.power((data[i] - data[ii]), 2)
+        #         denomerator = numpy.power((self.r_1 / 2),2)
+        #         ratio = numpy.exp(numpy.negative(numerator.sum() / denomerator))
+        #         ls = numpy.append(ls, ratio).sum()
+        #     i += 1
+        #     potential = numpy.append(potential, ls)
             
         # alternative...
-        # potential = numpy.sum(numpy.exp(numpy.negative( sp.spatial.distance_matrix(data, data) / numpy.power((self.r_1 / 2), 2) )), axis=1)   
+        potential = numpy.sum(numpy.exp(numpy.negative( sp.spatial.distance_matrix(data, data) / numpy.power((self.r_1 / 2), 2) )), axis=1)   
         return potential
-
     
     def update_mountain_function(self, mountain_function, max_mountain_function, center, data):
         '''
@@ -79,5 +82,6 @@ class SubtractiveClustering:
         denomerator = numpy.power((self.r_2 / 2), 2)
         ratio = numpy.exp(numpy.negative(numerator / denomerator))
         return mountain_function - max_mountain_function * ratio
+    
 
         
